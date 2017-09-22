00f7694ac3603b78bf40414ef1410a1ad631be29
gnsyxiang <gnsyxiang@163.com>
2017-09-22 21:55:04 +0800

    feat: for loops are replaced by macros

---
+++ src/misc.c
@@ -0,0 +1,25 @@
+/****************************************************************
+ * Filename: misc.c
+ * -------------------------------
+ * Copyright(C),
+ * Author: zhenquan.qiu
+ * Version: V1.0.0
+ * Last modified: 22/09 2017 21:54
+ * Description:
+ * 
+ * Change Log:
+ * NO.	Author		    Date		Modified
+ * 00	zhenquan.qiu	22/09 2017
+ ****************************************************************/
+#include <stdio.h>
+
+#define MISC_GB
+#include "misc.h"
+#undef MISC_GB
+
+
+void test(void)
+{
+    printf("test \n");
+}
+
