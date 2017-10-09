2d7dfa5aa09fd3f120d7d891b7606e5fad1e0d11
gnsyxiang <gnsyxiang@163.com>
2017-10-09 15:11:29 +0800

    feat: judgement byte order

--- include/misc_helper.h
+++ include/misc_helper.h
@@ -19,6 +19,9 @@
 extern "C" {
 #endif
 
+#include <stdio.h>
+#include <linux/swab.h>
+
 #ifndef MISC_HELPER_GB
 #define MISC_HELPER_EX extern
 #else
@@ -26,6 +29,26 @@ extern "C" {
 #endif
 
 #define FOR_ZERO_TO_LEN(len) for (int i = 0; i < (len); i++)
+
+// byte order processing function in <linux/swab.h>, eg: __swab16, __swab32
+// 内存地址从低地址向高地址增长，big endian是高位数据优先，即高位放在低地址。
+//                               little endian是低位数据优先，低位放在低地址。
+// 网络字节序采用大端。
+static inline int judgement_byte_order(void)
+{
+    int x = 0x01;
+    if (0x01 == *(char *)&x) {
+        printf("little endian \n");
+        return 1;
+    } else {
+        printf("big endian \n");
+        return 0;
+    }
+}
+#define is_little_endian()      (1 == judgement_byte_order())
+#define is_big_endian()         (0 == judgement_byte_order())
+
+
 
 MISC_HELPER_EX void test(void);
 MISC_HELPER_EX int random_num(int range);
