dd6d62ad3f1fc8f02488c0b580c4e006bd337787
gnsyxiang <gnsyxiang@163.com>
2017-12-08 23:19:33 +0800

    feat: store left bytes

--- test/socket_server.c
+++ test/socket_server.c
@@ -30,47 +30,63 @@ void handle_message(unsigned char *message, int len)
 	free(message);
 }
 
-void check_is_package(unsigned char *buf, handle_message_t handle_message)
+void check_is_package(unsigned char *buf, int ret, handle_message_t handle_message)
 {
 	int package_len;
-	int is_package_ok = 1;
+	unsigned char *pbuf;
+	static unsigned char buf_tmp[BUF_LEN];
+	static int store_left_bytes = 0;
 
-	while (*buf) {
-		while (*buf != FRONT_SYMBOL)
-			buf++;
+	pbuf = buf;
 
-		package_len = buf[1] | buf[2] << 8;
+	while (ret > 0) {
+		sleep(1);
 
-		if (buf[package_len] != TAIL_SYMBOL) {
-			buf++;
-			is_package_ok = 0;
+		while (*pbuf != FRONT_SYMBOL) {
+			pbuf++;
+			ret--;
 		}
 
-		if (is_package_ok) {
+		package_len = pbuf[1] | pbuf[2] << 8;
+
+		if (pbuf[package_len] != TAIL_SYMBOL && pbuf[package_len] != 0x0) {
+			pbuf++;
+			ret--;
+			continue;
+		}
+
+		if (pbuf[package_len] == TAIL_SYMBOL) {
 			unsigned char *message = (unsigned char *)malloc(package_len + 1);
-
-			memcpy(message, buf, package_len + 1);
-
+			memcpy(message, pbuf, package_len + 1);
 			handle_message(message, package_len + 1);
 
-			buf += package_len + 1;
+			pbuf += package_len + 1;
+			ret -= package_len + 1;
+		} else {
+			memcpy(buf_tmp, pbuf, ret);
+			print_hex(buf_tmp, ret);
+
+			store_left_bytes = ret;
+			ret = 0;
 		}
 	}
 }
 
 void *client_thread_callback(void *args)
 {    
+	int ret;
 	socket_t *client_sk = (socket_t *)args;
 
 	while(1) {
 		unsigned char buf[BUF_LEN] = {0};
 
-		if (socket_read(client_sk, (char *)buf, 16) <= 0) {
+		ret = socket_read(client_sk, (char *)buf, 16);
+		if (ret <= 0) {
 			printf("client socket close \n");
 			break;
 		}
 
-		check_is_package(buf, handle_message);
+		check_is_package(buf, ret, handle_message);
 	}
 
 	socket_clean_client(client_sk);
