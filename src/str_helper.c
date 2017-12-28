35deeaaf99b8e1e606892f349324eb8b4eabdccb
zhenquan.qiu <gnsyxiang@163.com>
2017-12-28 10:44:58 +0800

    feat: add str_helper.c file

---
+++ src/str_helper.c
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C) 2017 xxx Co., Ltd.
+ * All rights reserved.
+ * 
+ * @file    str_helper.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    28/12 2017 10:36
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        28/12 2017      create the file
+ * 
+ *     last modified: 28/12 2017 10:36
+ */
+
+/*
+ * #include <string.h>
+ * char *strdup(const char *s);
+ * 
+ * 作用: 将串拷贝到新建的位置处，返回新分配空间的首地址，失败返回NULL
+ * 注意: strdup在内部调用malloc()为变量分配内存，不需要使用返回的字符串时，
+ *       需要用free()释放相应的内存空间，否则会造成内存泄露
+ */
