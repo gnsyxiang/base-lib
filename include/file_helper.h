6155a5a5328d493165a15c9125c82a0846965ba5
zhenquan.qiu <gnsyxiang@163.com>
2018-01-25 18:47:21 +0800

    refactor: mv parse_cmd.c from src to test

--- include/file_helper.h
+++ include/file_helper.h
@@ -55,6 +55,7 @@ static inline FILE *fopen_l(const char *path, const char *mode)
 FILE_HELPER_EX int file_close_on_exec(int fd);
 FILE_HELPER_EX int file_set_nonblocking(int fd);
 FILE_HELPER_EX ssize_t file_read(int fd, void *buf, size_t cnt);
+FILE_HELPER_EX ssize_t file_read_timeout(int fd, void *buf, size_t cnt, size_t timeout_ms);
 FILE_HELPER_EX ssize_t file_write(int fd, const void *buf, size_t cnt);
 
 
