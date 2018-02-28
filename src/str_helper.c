beecba1deeaeee18c55d57cdb87fae7791d277c6
zhenquan.qiu <gnsyxiang@163.com>
2018-02-28 14:47:34 +0800

    feat: [str] add str_replace_substr() new func

--- src/str_helper.c
+++ src/str_helper.c
@@ -21,11 +21,17 @@
 #include <string.h>
 
 #include "misc_helper.h"
+#include "mem_helper.h"
 
 #define STR_HELPER_GB
 #include "str_helper.h"
 #undef STR_HELPER_GB
 
+/**
+ * @brief first and end exchange string
+ *
+ * @param str: the switched string
+ */
 void str_swap_first_and_end(char *str)
 {
 	int len = strlen(str);
@@ -37,6 +43,12 @@ void str_swap_first_and_end(char *str)
 	}
 }
 
+/**
+ * @brief get the extension name
+ *
+ * @param file_name: file path
+ * @param ext_name: the extension name of the file 
+ */
 void str_get_file_extension_name(const char *file_name, char *ext_name)  
 {  
 	const char *p = file_name + strlen(file_name) - 1;
@@ -49,6 +61,45 @@ void str_get_file_extension_name(const char *file_name, char *ext_name)
 
 	str_swap_first_and_end(pext_name);
 }  
+
+
+/**
+ * @brief replace the specified string in a string 
+ *
+ * @param src: original string
+ * @param oldstr: old string
+ * @param newstr: new string
+ * @param num: replace times
+ *
+ * @return return the new string after the replacement
+ */
+const char *str_replace_substr(const char *src, const char *oldstr, const char *newstr, int num)
+{
+	const char *dst = src;
+	char *tmp = NULL;
+	char *find_oldstr;
+	int replace_cnt = 0;
+
+	if (strcmp(oldstr, newstr) == 0) {
+		dst = strdup(tmp);
+		return dst;
+	}
+
+	while ((find_oldstr = strstr(dst, oldstr)) && replace_cnt++ < num) {
+		tmp = alloc_mem(strlen(dst) + (strlen(newstr) + strlen(oldstr)) + 1);
+
+		strncpy(tmp, dst, find_oldstr - dst);
+		tmp[find_oldstr - dst] = '\0';
+
+		strcat(tmp, newstr);
+
+		strcat(tmp, find_oldstr + strlen(oldstr));
+
+		dst = strdup(tmp);
+	}
+
+	return dst;
+}
 
 
 
