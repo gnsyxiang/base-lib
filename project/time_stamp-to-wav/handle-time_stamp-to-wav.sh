1dc05f4d9d93efbae367380a83e13ba76102a2a5
zhenquan.qiu <gnsyxiang@163.com>
2018-06-21 18:47:04 +0800

    fix: [wav] add some var to sh

--- project/time_stamp-to-wav/handle-time_stamp-to-wav.sh
+++ project/time_stamp-to-wav/handle-time_stamp-to-wav.sh
@@ -20,6 +20,12 @@
 #     last modified: 21/06 2018 14:25
 # ===============================================================
 
+wav_path=wav
+src_path=${wav_path}/src
+dst_path=${wav_path}/dst
+
+src_file_name=${src_path}/vad-test.wav
+
 if [ $# -ne 1 ]; then
 	echo "eg: $0 time_stamp.txt"
 	exit 1
@@ -28,12 +34,12 @@ elif [ $1 == "-h" ]; then
 	exit 1
 fi
 
-awk 'BEGIN{i=0;} \
-	/time:/ \
-	{printf "%03d.wav %s %s \n", i++, $2, $3}' \
-	$1 \
-	| \
-	awk '{system("./main -t vad-test.wav -f "$1" -s "$2" -p "$3"")}'
+if [ ! -d ${dst_path} ]; then
+	mkdir -p ${dst_path} 
+fi
+
+awk 'BEGIN{i=0;} /time:/ {printf "%s/%03d.wav %s %s \n", "'${dst_path}'", i++, $2, $3}'  $1 | \
+awk '{system("./main -t '${src_file_name}' -f "$1" -s "$2" -p "$3"")}'
 
 
 
