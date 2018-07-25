e05d16e893c3f5fbc9684e04f752c50fb29855a1
zhenquan.qiu <gnsyxiang@163.com>
2018-07-25 09:41:50 +0800

    feat: [autotools] add a demo

---
+++ project/autotools/hello.c
@@ -0,0 +1,28 @@
+/**
+ * 
+ * Release under GPLv2.
+ * 
+ * @file    hello.c
+ * @brief   
+ * @author  gnsyxiang <gnsyxiang@163.com>
+ * @date    25/07 2018 11:16
+ * @version v0.0.1
+ * 
+ * @since    note
+ * @note     note
+ * 
+ *     change log:
+ *     NO.     Author              Date            Modified
+ *     00      zhenquan.qiu        25/07 2018      create the file
+ * 
+ *     last modified: 25/07 2018 11:16
+ */
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	printf("hello world \n");
+	
+	return 0;
+}
+
